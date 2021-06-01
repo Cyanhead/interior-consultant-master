39a40378103ed14d9e21315a733764b54fe47513
Bunmi <bunmioye09@gmail.com>
2021-06-01 11:26:40 +0100

    add all files

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log('the js works');
+
+let menuBtn = document.querySelector('.hamburg');
+let closeMenuBtn = document.querySelector('.close-x');
+let menuShade = document.querySelector('div.mobile-nav');
+
+menuBtn.addEventListener('click', () => {
+  menuShade.classList.add('show');
+  console.log('click menu');
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  menuShade.classList.remove('show');
+  console.log('click close menu');
+});
